443f9da5096ffcdb6f990d234b1908e8461db461
Rustem <t.rustem17@gmail.com>
2020-07-06 21:58:10 +0600

    undone 5

---
+++ week1/ksrc2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+void dfs () {
+
+    
+}
+
+int main () {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        
+        char a[n][m];
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                cin >> a[i][j];
+            }
+        }
+
+// dfs
+
+        if () {
+            cout << "-1\n";
+        } else {
+// topolohical sorting using dfs
+        }    
+    }
+
+}
