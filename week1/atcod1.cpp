40bc0025d7490f135e2e0fec56ad063ee2234ddc
Rustem <t.rustem17@gmail.com>
2020-07-07 22:08:03 +0600

    done 14 (r)

--- week1/atcod1.cpp
+++ week1/atcod1.cpp
@@ -5,35 +5,36 @@ using namespace std;
 #define ll long long
 #define pb push_back
 
+const int MOD = 1E9 + 7;
+int a[200005];
 
 int main () {
 
-    
-    ll MOD = 1E9 + 7;
+    int n, k;
 
-    ll n, k;
     cin >> n >> k;
 
-    vector <int> a;
-    vectro <int> na;
-
-    for (int i = 0; i < n; ++i) {
-        ll ttt;
-        cin >> ttt;
-        if (ttt < 0) na[kkk++] = ttt;
-        else a[lll++] = ttt;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
     }
 
-    sort(a.begin(), a.end());
-    sort(na.begin(), na.end());
+    sort(a + 1, a + n + 1);
+    int r = 1;
 
-    ll prod = 1;
+    if (k == n || a[n] < 0) {
+        if (k & 1) for (int i = n - k + 1; i <= n; ++i) res = 1LL * r * a[i] % MOD;
+        else for (int i = 1; i <= k; ++i) r = 1LL * r * a[i] % MOD;
+    } else {
+        int l = 1, rr = n;
+        if (k & 1) r = 1LL * r * a[r] % MOD, rr--, k--;
 
-    vector <int> nna;
-
-    for (int i = 0; i < na.size(); i += 2) {
-        nna.pb();
+        while (k) {
+            if (1LL * a[l] * a[l + 1] > 1LL * a[rr] * a[rr - 1]) r = 1LL * r * a[l] % MOD, l+= 2;
+            else r = 1LL * r * a[rr] % MOD * a[rr - 1] % MOD, rr -= 2;
+            k -= 2;
+        }
     }
 
-    cout << prod % MOD << "\n";
+    r = (r + MOD) % MOD;
+    cout << r << "\n";
 }
