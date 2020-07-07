c85f3456720f44ca9066cd58e8408e6c4f28ea51
Rustem <t.rustem17@gmail.com>
2020-07-07 21:58:09 +0600

    done 13 (r)

--- week1/ksrc2.cpp
+++ week1/ksrc2.cpp
@@ -5,9 +5,23 @@ using namespace std;
 #define ll long long
 #define pb push_back
 
-void dfs () {
+string s[30], ans;
+bool bb;
+bool vis[26], act[26];
+vector <int> adj[26];
 
-    
+void dfs (int u) {
+
+    act[u] = 1;
+    vis[u] = 1;
+    for (int v : adj[u]) {
+        if (act[v] && v^u) bb = 1;
+        else {
+            if (!vis[v]) dfs(v);
+        } 
+    }
+    act[u] = 0;
+    ans += (char) (u + 'A');
 }
 
 int main () {
@@ -18,21 +32,36 @@ int main () {
     while (t--) {
         int n, m;
         cin >> n >> m;
+        set< char> tt; 
         
-        char a[n][m];
-
         for (int i = 0; i < n; ++i) {
+            cin >> s[i];
             for (int j = 0; j < m; ++j) {
-                cin >> a[i][j];
+                tt.insert(s[i][j]);
+            }
+
+            if (i) {
+                for (int j = 0; j < m; ++j) {
+                    adj[s[i - 1][j] - 'A'].pb(s[i][j] - 'A');
+                }
             }
         }
 
-// dfs
+        memset(vis, 0, 26);
+        memset(act, 0, 26);
 
-        if () {
+        ans = "";
+        bb = 0;
+
+        for (char c : tt) {
+            if (!vis[c - 'A']) dfs(c - 'A');
+        }
+
+        if (bb) {
             cout << "-1\n";
         } else {
-// topolohical sorting using dfs
+            cout << ans << "\n";
+            for (int i = 0; i < 26; ++i) adj[i].clear();
         }    
     }
 
