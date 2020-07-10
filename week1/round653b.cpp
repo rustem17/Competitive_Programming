5426113339979ce72dfb8ecfb3bdc0a793cd9a4d
Rustem <t.rustem17@gmail.com>
2020-07-10 14:09:48 +0600

    redone 5

--- week1/round653b.cpp
+++ week1/round653b.cpp
@@ -10,34 +10,25 @@ int main () {
 
     int t;
     cin >> t;
-    while (t--) {
 
-        ll n;
+    while (t--) {
+        int n;
         cin >> n;
 
-        int c2 = 0;
-        int c3 = 0;
+        int c2 = 0, c3 = 0;
 
-        bool ind = 0;
-
-        while (!ind) {
-
-            ind = 1;
-
-            if (n % 2 == 0) {
-                c2++;
-                n /= 2;
-                ind = 0;
-            } else if (n % 3 == 0) {
-                c3++;
-                n /= 3;
-                ind = 0;
-            }
+        while (n % 2== 0) {
+            n /= 2;
+            c2++;
         }
 
-        if (n == 1) {    
-            if (c2 > c3) cout << "-1\n";
-            else cout << c3 - c2 + c3 << "\n";
+        while (n % 3 == 0) {
+            n /= 3;
+            c3++;
+        }
+    
+        if (n == 1 && c2 <= c3) {
+            cout << 2 * c3 - c2 << "\n";
         } else {
             cout << "-1\n";
         }
