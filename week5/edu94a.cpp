471981c798bb6e46dda491986bd88edb8d272f22
Rustem Turtayev <40733085+rustem17@users.noreply.github.com>
2020-08-27 23:35:32 +0600

    done 1

---
+++ week5/edu94a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+
+    int t;
+    cin >> t;
+
+
+    while (t--) {
+
+        int n;
+        string c;
+
+        cin >> n >> c;
+
+        for (int i = 0; i < n; ++i) cout << c[n - 1];
+        cout << "\n";
+    }
+}
