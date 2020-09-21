5efe847b2562f6ba1bf771695d5c95094731f092
Rustem Turtayev <40733085+rustem17@users.noreply.github.com>
2020-09-21 23:14:14 +0600

    done 4

---
+++ week9/edu82a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        string s;
+        cin >> s;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != '0') cout << s[i];
+        }
+
+        cout << "\n";
+    }
+}
