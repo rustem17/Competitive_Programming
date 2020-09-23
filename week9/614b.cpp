c57163f6b4da94aacd86ab70947317103e00407e
Rustem Turtayev <40733085+rustem17@users.noreply.github.com>
2020-09-23 22:50:58 +0600

    done 2

---
+++ week9/614b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+
+    int n;
+    cin >> n;
+
+    double ans = 0.0;
+    for (int i = 1; i <= n; ++i) ans += 1.0 / i;
+
+    cout << fixed << setprecision(12) << ans << "\n";
+}
